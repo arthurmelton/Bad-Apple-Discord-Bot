04e2578fecdfa9f3877920186dff3ad5320e75ae
Arthur Melton <amtitan@icloud.com>
2021-04-12 17:47:40 -0400

    console logs video

--- src/bot.js
+++ src/bot.js
@@ -3,5 +3,17 @@ fs.readFile('input.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+  var newData = data.replaceAll("1", "##").replaceAll("0", "||     ||");
+  const lines = data.split("\n");
+  //const lines = newData.split("\n");
+  var line = 0;
+  for (var i = 0; i < 4382; i++) {
+    setTimeout(() => {
+      for (var c = 0; c < 28; c++) {
+        console.log(lines[line]);
+        line++;
+      }
+      console.log("====================================");
+    }, 41 * i);
+  }
 });
